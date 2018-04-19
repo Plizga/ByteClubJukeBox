5e4e672f82051fd64c45a1b3d1b5512bcc84a762
Jacker713 <32367528+Jacker71300@users.noreply.github.com>
2018-04-19 14:27:04 -0400

    Added WMP and started wav format player

--- Jukebox/Jukebox/Form1.Designer.cs
+++ Jukebox/Jukebox/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.SongSelector = new System.Windows.Forms.ComboBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Location = new System.Drawing.Point(239, 74);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(75, 23);
+            this.PlayBtn.TabIndex = 0;
+            this.PlayBtn.Text = "Play";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // SongSelector
+            // 
+            this.SongSelector.FormattingEnabled = true;
+            this.SongSelector.Location = new System.Drawing.Point(498, 12);
+            this.SongSelector.Name = "SongSelector";
+            this.SongSelector.Size = new System.Drawing.Size(403, 21);
+            this.SongSelector.TabIndex = 1;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(299, 159);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(522, 132);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 449);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.SongSelector);
+            this.Controls.Add(this.PlayBtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button PlayBtn;
+        private System.Windows.Forms.ComboBox SongSelector;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
--- Jukebox/Jukebox/Form1.cs
+++ Jukebox/Jukebox/Form1.cs
@@ -6,15 +6,35 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Jukebox
 {
+
     public partial class Form1 : Form
     {
+        private SoundPlayer Player = new SoundPlayer();
+
         public Form1()
         {
             InitializeComponent();
+            //Player.SoundLocation = System.IO.Directory.GetCurrentDirectory().Substring(0, System.IO.Directory.GetCurrentDirectory().IndexOf("/bin")) + "/resources/" + SongSelector.Text + ".wav";
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            Player.Play(); 
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Player.SoundLocation = System.IO.Directory.GetCurrentDirectory().Substring(0, System.IO.Directory.GetCurrentDirectory().IndexOf("/bin")) + "/resources/" + SongSelector.Text + ".wav";
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
