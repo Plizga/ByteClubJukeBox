a3f80e39857e4c85f4966805daf6ec13415b1d16
Jacker713 <32367528+Jacker71300@users.noreply.github.com>
2018-04-20 14:32:05 -0400

    fixed file scraper

--- Jukebox/Jukebox/Form1.cs
+++ Jukebox/Jukebox/Form1.cs
@@ -16,13 +16,17 @@ namespace Jukebox
     {
         private WMPLib.WindowsMediaPlayer Player = new WMPLib.WindowsMediaPlayer();
         private string Url;
-        private string fileDirectory = System.IO.Directory.GetCurrentDirectory().Substring(0, System.IO.Directory.GetCurrentDirectory().IndexOf("/Jukebox") + 64) + "Assets/AudioFiles/";
+        private string fileDirectory = System.IO.Directory.GetCurrentDirectory().Substring(0, System.IO.Directory.GetCurrentDirectory().IndexOf("\\bin")) + "/Assets/AudioFiles/";
         private List<string> files = new List<string>();
 
         public Form1()
         {
             InitializeComponent();
 
+            for (int i = 0; i < files.Count; i++)
+            {
+                files[i] = files[i].Replace("\\", "/");
+            }
 
             files.AddRange(System.IO.Directory.GetFiles(fileDirectory, "*.mp3"));
 
